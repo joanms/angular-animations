6d6c69e52c3eb79d5dea4a765499d86ef99c59fa
Joan Sweeney <joanms@eircom.net>
2020-05-31 17:50:58 +0100

    Add void state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,14 +34,37 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       transition('highlighted => normal', animate(800)),
       transition('shrunken <=> *', [
         style({
-          backgroundColor: 'orange'
+          'background-color': 'orange'
         }),
         animate(1000, style({
           borderRadius: '50px'
         })),
         animate(500)
       ])
-    ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        opacity: 1,
+        transform: 'translateX(100px)'
+      })),
+      transition('void <=> *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100)'
+        }),  
+        animate(300)
+      ]),
+      transition('* <=> void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ]),
+    ]),
   ]
 })
 export class AppComponent {
