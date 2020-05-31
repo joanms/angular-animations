ef4a3a89dfaa016fa246f6edc098b8017f2bd22e
Joan Sweeney <joanms@eircom.net>
2020-05-31 15:02:31 +0100

    Add code to switch between states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { trigger, state, style } from '@angular/animations';
   animations: [
     trigger('divState', [
       state('normal', style({
-        'background-color': 'red',
+        backgroundColor: 'red',
         transform: 'translateX(0)'
       })),
       state('highlighted', style({
@@ -20,6 +20,10 @@ import { trigger, state, style } from '@angular/animations';
 export class AppComponent {
   state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
+
+  onAnimate() {
+    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+  }
 
     onAdd(item) {
       this.list.push(item);
