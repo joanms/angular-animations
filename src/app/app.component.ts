9d1adb4d5f171216747f4297aa093e3743af6e41
Joan Sweeney <joanms@eircom.net>
2020-05-31 15:31:52 +0100

    Add advanced animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,24 +14,47 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         backgroundColor: 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300)),
+      // transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800))
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
   }
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
 
-    onDelete(item) {
-      this.list.splice(this.list.indexOf(item), 1);
-    }
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
 }
